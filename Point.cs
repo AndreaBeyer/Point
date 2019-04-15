a8462b865598472afb5a2a1adffcd0ead9f805cb
AndreaBeyer <48089121+AndreaBeyer@users.noreply.github.com>
2019-04-15 10:35:46 +0200

    Initial commit

---
+++ Point.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Point
+{
+    public class Point
+    {
+        private float x;
+        private float y;
+
+        public Point()
+        {
+            x = 0;
+            y = 0;
+        }
+
+        public Point(float _x,float _y)
+        {
+            x = _x;
+            y = _y;
+        }
+
+        public void Deplacer(float _x, float _y)
+        {
+            x += _x;
+            y += _y;
+        }
+
+        public void Permuter()
+        {
+            float temp = x;
+            x = y;
+            y = temp;
+        }
+
+        public Point SymetrieAxeAbscisse()
+        {
+            Point p = new Point(x,-y);
+            return p;
+        }
+
+        public Point SymetrieAxeOrdonnee()
+        {
+            Point p = new Point(-x,y);
+            return p;
+        }
+
+        public Point SymetrieOrigine()
+        {
+            Point p = new Point(-x, -y);
+            return p;
+        }
+
+        public void GetString()
+        {
+            Console.WriteLine("mon abscisse est de : {0} mon ordonnee est de : {1}", x, y);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Point
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point();
+            Point p = new Point(140,90);
+            p.GetString();
+            p1.GetString();
+            p1.Deplacer(10.25f, -50.99f);
+            p1.GetString();
+            p.Deplacer(-120, 20);
+            p.GetString();
+            Point pSymetriqueOrdonee = new Point();
+            pSymetriqueOrdonee = p.SymetrieAxeOrdonnee();
+            pSymetriqueOrdonee.GetString();
+            Point pSymetriqueAbscisse = new Point();
+            pSymetriqueAbscisse = p.SymetrieAxeAbscisse();
+            pSymetriqueAbscisse.GetString();
+            Point pSymetriqueOrigine = new Point();
+            pSymetriqueOrigine = p.SymetrieOrigine();
+            pSymetriqueOrigine.GetString();
+            p.Permuter();
+            p.GetString();
+        }
+    }
+}
